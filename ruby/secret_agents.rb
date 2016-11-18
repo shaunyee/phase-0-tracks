58c65a49c0351e1cd08ea0bb6b4dc213a954eca6
“Shaun <shaunwyee@gmail.com>
2016-11-17 20:27:01 -0800

    finished pair, missing reslease 4

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,35 @@
+
+def encrypt(str)
+	i=0
+	while i<str.length
+	if str[i]=="z"
+	  print str[i]="a"
+	  i+=1
+	end
+		print str[i].next
+		i+=1
+end
+	end
+def decrypt(str)
+  i=0
+  while i<str.length
+  if str[i]=="a"
+	  print str[i]="z"
+	  i+=1
+	end
+  print (str[i].ord-1).chr
+  i+=1
+end
+end
+puts "would you like to decrypt or encrypt your password?"
+input=gets.chomp
+puts "Enter your password"
+password=gets.chomp
+  if input=="decrypt"
+    decrypt(password)
+    elsif
+    input=="encrypt"
+    encrypt(password)
+  end
+
+  #release 4 did not work for us.  We were getting an error about defining a method for length.
