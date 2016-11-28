643e6d26d570fa6c3291d5cb7c2d964553d36f30
“Shaun <shaunwyee@gmail.com>
2016-11-27 20:41:34 -0800

    finished 5.6

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -25,3 +25,27 @@ fib(6)
 
 fib(100)[-1]
 
+#Insertion Method Pseudocode:
+#
+# - Define a method that takes an array of integers as a parameter.
+# - Iterate through the array of integers by defining a range that
+#   starts at the zero index and goes up to the last integer in the
+#   array.
+# - The integers in the array will be sorted in place:
+#   - At each index (starting with 1), the method checks the integer
+#     value against the largest value in the sorted list (which would
+#     be the value to the left of it as indices are checked)
+#   - If the integer is larger than the number to the left, it leaves
+#     in place and moves to the next integer
+#   - If smaller, it finds the correct position in the sorted list,
+#     shifts the larger values up and inserts the integer in the correct
+#     position
+# - After iterating through the entire array it returns the sorted
+#   array# Sorting Method:def insertion_sort(array)
+ for i in 1..(array.length - 1)
+   while i > 0 && array[i-1] > array[i]
+     array[i], array[i-1] = array[i-1], array[i]
+     i -= 1
+   end
+ end
+end
