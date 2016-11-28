ec083c86e1da690452b4fc9f0ce4f1f822fc3f04
“Shaun <shaunwyee@gmail.com>
2016-11-27 20:59:35 -0800

    added pseudocode

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,4 +1,27 @@
-#interior Design Customer info
+# #interior Design Customer info
+# pseudocode
+# will design an array and hash that will store data for the form
+# 	-ask what the user's name is
+# 		-store the name into the array
+# 	-ask the user their age
+# 		-store information into array
+# 	-ask the user how many children they have
+# 		-store answer in array	
+# 	-ask the user what theme they would like
+# 		-store answer in array
+# 	-ask user where they live
+# 		-store answer in array
+# 	-ask the user for their email
+# 		-store answer in array
+# move the information into a hash
+# 	-print out the hash
+# 	-ask the user if there is anything they would like to change
+# 		-run a method and loop that asks them for what they would like to change
+# 			-turn the answers of the hash into a string
+# 			-return the changes in the hash
+# 		-print the new updated hash
+
+
 
 customer_info =[]
 
