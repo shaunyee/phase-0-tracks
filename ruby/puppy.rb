5657e1b59ac23639aebf87b69093a166eac949b9
“Shaun <shaunwyee@gmail.com>
2016-11-30 19:35:51 -0800

    design class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+#Puppy
+#Species--------
+#Golden Retriever
+
+#Characteristics------
+#Fur color: blonde
+#Friendly :yes
+#Eye Color: Varies
+
+#Tricks------
+#Jump
+#Roll Over
+#Shake
+#Play Dead
+#Kiss
