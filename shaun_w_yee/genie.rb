d927624098a4fd37cec78a079bfd46f9e64cfb2f
“Shaun <shaunwyee@gmail.com>
2016-12-12 00:41:36 -0800

    added grant wish method

--- shaun_w_yee/genie.rb
+++ shaun_w_yee/genie.rb
@@ -1,19 +1,42 @@
-class Genie
-	attr_reader :age
-	attr_accessor :name
+class Genie 
+  attr_accessor :name
+  attr_reader :age
+  attr_reader :submitted_wishes
+  
+  private :submitted_wishes
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @inLamp = true
+    @submitted_wishes = Hash.new
+  end
+  
+  def exit_lamp
+    @inLamp = false
+  end
+  
+  def enter_lamp
+    @inLamp = true
+  end
+  
+  def grant_wish(wish)
+    exit_lamp()
+    granted = nil
+    
+    if @submitted_wishes.length < 3
+      granted = true
+      p "Your wish of #{wish} has been granted!"
+    else
+      granted = false
+      p "Sorry, I've already granted three wishes today!"
+    end
+    
+    @submitted_wishes[wish] = granted
 
-def initialize(name,age)
-	@name = name
-	@age = age
-end
-
-def enter_lamp
-	puts "the genie is currently inside the lamp"
-end
-def exit_lamp
-	puts "the genie is currently outside the lamp"
-end
-end
-
-genie1 = Genie.new("Bob", "3218")
-genie1.enter_lamp
+    
+    enter_lamp()
+    granted
+  end
+  
+ 
