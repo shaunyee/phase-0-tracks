450a53486613c8d931a9debd198be329e45a4506
“Shaun <shaunwyee@gmail.com>
2016-12-11 22:18:42 -0800

    added status

--- shaun_w_yee/genie.rb
+++ shaun_w_yee/genie.rb
@@ -1,5 +1,5 @@
-class genie
-	att_reader :age
+class Genie
+	attr_reader :age
 	attr_accessor :name
 
 def initialize(name,age)
@@ -7,3 +7,13 @@ def initialize(name,age)
 	@age = age
 end
 
+def enter_lamp
+	puts "the genie is currently inside the lamp"
+end
+def exit_lamp
+	puts "the genie is currently outside the lamp"
+end
+end
+
+genie1 = Genie.new("Bob", "3218")
+genie1.enter_lamp
