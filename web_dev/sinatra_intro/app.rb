b202f631131c78e15482fa3a677c3304ef221216
“Shaun <shaunwyee@gmail.com>
2017-01-02 21:11:52 -0800

    finished 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -6,7 +6,19 @@ db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
 # write a basic GET route
+get '/contact'do
+"www.google.com"
+end
 # add a query parameter
+get '/great_job/:name' do
+  name = params [:name]
+  "Good job, #{name}!"
+end
+
+get '/:number_1/plus/:number_2' do
+"#{params[:number_1].to_i + params[:number_2].to_i}"
+end
+
 # GET /
 get '/' do
   "#{params[:name]} is #{params[:age]} years old."
@@ -44,3 +56,19 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+# Release 1
+
+# 1. Is Sinatra the only web app library in Ruby? What are some others?
+# Cuba, Pandrino, Lotus, Rails
+
+# 2. Are SQLite and the sqlite3 gem your only options for using a database with Sinatra? What are some others?
+# activerecord, fwitter 3, heroku
+
+# 3.What is meant by the term web stack?
+# It refers to the components or technologies/languages/operating systems used to build a website.
+
+
+
+
+
